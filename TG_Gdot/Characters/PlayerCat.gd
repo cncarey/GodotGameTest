e00a3f63dcbc122313189e9bee8a8db49f3f8d9b
cncarey <courtneykrider@yahoo.com>
2023-03-25 19:26:15 -0400

    #5 added character animation #added a tileMap
    #5 Added in basic character animations and added more assets
    
    #1 started making a tilemap

--- TG_Gdot/Characters/PlayerCat.gd
+++ TG_Gdot/Characters/PlayerCat.gd
@@ -1,18 +1,45 @@
 extends CharacterBody2D
 
 @export var moveSpeed : float  = 100
+var movement = Vector2();
+@export var statingDirection : Vector2 = Vector2(0, 0)
+# parameters/Idle/blend_position
+var catIdlePB : StringName = "parameters/Idle/blend_position"
+var catWalkPB : StringName = "parameters/Walk/blend_position"
+var walk : StringName = "Walk"
+var idle : StringName = "Idle"
+
+@onready var animationTree = $AnimationTree
+@onready var animationMode = animationTree.get("parameters/playback")
+
+
+func _ready():
+	animationTree.set(catIdlePB, statingDirection)
 
 func _physics_process(_delta):
+	
 	#Get input direction
 	var inputDirection = Vector2(
 		Input.get_action_strength("right") - Input.get_action_strength("left"),
 		Input.get_action_strength("down") - Input.get_action_strength("up")
-	)
+	)	
 	
-	print(inputDirection)
+	updateAnimationParams(inputDirection)
 	
 	#Update velocity
-	velocity = inputDirection * moveSpeed
+	velocity = inputDirection.normalized() * moveSpeed
 	
 	#move and slide function uses velocity body to move character on map
 	move_and_slide()
+	setState()
+	
+func updateAnimationParams(movingInput : Vector2):	
+	if(movingInput != Vector2.ZERO):
+		animationTree.set(catIdlePB, movingInput)
+		animationTree.set(catWalkPB, movingInput)
+		
+func setState():
+	if(velocity != Vector2.ZERO):
+		animationMode.travel(walk)
+	else:
+		animationMode.travel(idle)	
